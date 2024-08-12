838f290d578adcd6b640055539f770919979323e
Murtazaliev <murad.murtazaliev@gmail.com>
2024-08-12 17:05:19 +0300

    fixed test task

--- DefinitionOfTriangle.App/DefinitionOfTriangleService.cs
+++ DefinitionOfTriangle.App/DefinitionOfTriangleService.cs
@@ -5,22 +5,21 @@ using DefinitionOfTriangle.App.Models;
 public class DefinitionOfTriangleService : IDefinitionOfTriangleService
 {
     public DefinitionOfTriangleResult DetermineTriangleType(DefinitionOfTriangleRequest request)
-    {
-        if (request.SideA + request.SideB <= request.SideC
-            || request.SideA + request.SideC <= request.SideB
-            || request.SideB + request.SideC <= request.SideA)
-        {
-            return new DefinitionOfTriangleResult { ResultType = ResultType.IncorrectValues, Description = "Стороны не могут образовать треугольник" };
-        }
+    {       
 
         double[] sides = { request.SideA, request.SideB, request.SideC };
         Array.Sort(sides);
+
 
         double a = sides[0];
         double b = sides[1];
         double c = sides[2];
 
-        if (a * a + b * b == c * c)
+        if (a + b <= c)
+        {
+            return new DefinitionOfTriangleResult { ResultType = ResultType.IncorrectValues, Description = "Стороны не могут образовать треугольник" };
+        }
+        else if (a * a + b * b == c * c)
         {
             return new DefinitionOfTriangleResult { ResultType = ResultType.Rectangular, Description = "Треугольник прямоугольный" };
         }
